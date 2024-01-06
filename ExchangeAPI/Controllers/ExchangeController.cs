1b37701154b43f67441f06b76c69bbccf39ba64f
Eren Çalbay <erencalbayfb@hotmail.com>
2024-01-07 02:07:13 +0300

    Init: İster 3 ve 4 yapıldı.

---
+++ ExchangeAPI/Controllers/ExchangeController.cs
@@ -0,0 +1,33 @@
+﻿using ExchangeAPI.Helper;
+using ExchangeAPI.Models.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections;
+using System.Reflection;
+using System.Text.Json;
+
+namespace ExchangeAPI.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class ExchangeController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetSupportedExchanges()
+        {
+            var APIresult = ExchangeHelper.GetDeserializeAPI();
+            var result = ExchangeHelper.GetExchangeCurrencies(APIresult);
+            return Ok(result);
+        }
+
+        [HttpGet("{amount}/{typeOne}/{typeTwo}")]
+        public IActionResult GetExchangeForValueTwoExchange(double amount, string typeOne = "USD", string typeTwo = "TRY")
+        {
+            var APIresult = ExchangeHelper.GetDeserializeAPI(typeOne);
+            var result = ExchangeHelper.GetExchangePropertyValue(APIresult.conversion_rates, typeTwo) * amount;
+            return Ok(result);
+        }
+    }
+}
---
+++ ExchangeAPI/Helper/ExchangeHelper.cs
@@ -0,0 +1,38 @@
+﻿using ExchangeAPI.Models.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Newtonsoft.Json;
+using System;
+using System.Reflection;
+using System.Text.Json.Nodes;
+
+namespace ExchangeAPI.Helper
+{
+    public class ExchangeHelper
+    {
+        public static double GetExchangePropertyValue(ConversionRate conversionRate, string propertyName)
+        {
+            PropertyInfo property = conversionRate.GetType().GetProperty(propertyName);
+            if (property != null)
+            {
+                return (double)(property.GetValue(conversionRate) ?? 0.0);
+            }
+            return 0.0;
+        }
+        public static ExchangeEntity GetDeserializeAPI(string typeOne = "None")
+        {
+            String URLString = "https://v6.exchangerate-api.com/v6/c47ca772c6a15693dbcfc070/latest/USD";
+            if (typeOne!="None")
+                URLString = "https://v6.exchangerate-api.com/v6/c47ca772c6a15693dbcfc070/latest/" + typeOne;
+            var webClient = new System.Net.WebClient();
+            var json = webClient.DownloadString(URLString);
+            ExchangeEntity Test = JsonConvert.DeserializeObject<ExchangeEntity>(json);
+            return(Test);
+        }
+
+        public static ConversionRate GetExchangeCurrencies(ExchangeEntity exchangeEntity)
+        {
+            PropertyInfo property = exchangeEntity.GetType().GetProperty("conversion_rates");
+            return (ConversionRate)(property.GetValue(exchangeEntity));
+        }
+    }
+}
