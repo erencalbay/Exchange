ac345db372f5cf1e6df224f9b36ced125890c58f
Eren Çalbay <erencalbayfb@hotmail.com>
2024-01-11 20:29:06 +0300

    Feat: Fluentvalidation

---
+++ ExchangeAPI/Models/ViewModel/GetExchangeForValuteTwoExchangeModelValidation.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ExchangeAPI.Models.ViewModel
+{
+    public class GetExchangeForValuteTwoExchangeModelValidation : AbstractValidator<GetExchangeForValuteTwoExchangeModel>
+    {
+        public GetExchangeForValuteTwoExchangeModelValidation()
+        {
+            RuleFor(x => x.Amount).NotEmpty().GreaterThan(0).WithMessage("Amount must be greater than 0");
+            RuleFor(x => x.CurrencyTwo).IsInEnum().WithMessage("Unsupported Currency");
+            RuleFor(x => x.CurrencyOne).IsInEnum().WithMessage("Unsupported Currency");
+        }
+    }
+}
--- ExchangeAPI/Program.cs
+++ ExchangeAPI/Program.cs
@@ -1,11 +1,15 @@
+using ExchangeAPI.Models.ViewModel;
+using FluentValidation.AspNetCore;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddJsonOptions(options =>
- options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+ options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()))
+    .AddFluentValidation(configuration => configuration.RegisterValidatorsFromAssemblyContaining<GetExchangeForValuteTwoExchangeModelValidation>());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
